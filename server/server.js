88342994c2052e615b3d31eb566b555d6809351a
Travis Lang <tlang50@icloud.com>
2018-11-14 10:51:35 -0600

    add logic for random quotes function

--- server/server.js
+++ server/server.js
@@ -6,11 +6,28 @@ const app = express( );
 // set port
 const PORT = 5000;
 
+let quotes = [
+    'work is the curse of the drinking class',
+    'the only thing to fear is fear itself',
+    'fool me once shame on me, but teach a man to fool me and...'
+    ];
+
+// setup basic route
 app.get( '/quote', function( req, res ){
-    res.send( 'hello' );
+    res.send( getQuote( ) )
 })
 
-
+let index = 0;
+// return on quote
+// not the same every time
+function getQuote( ){
+    let quoteToReturn = quotes[ index ];
+    index++;
+    if( index >= quotes.length ){
+        index = 0;
+    }
+    return quoteToReturn;
+}
 
 
 // start up server on port
