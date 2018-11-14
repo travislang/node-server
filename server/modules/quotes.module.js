d08ab4e632fdd024a552d03de7cc4943e024d162
Travis Lang <tlang50@icloud.com>
2018-11-14 10:55:33 -0600

    setup module for function logic

---
+++ server/modules/quotes.module.js
@@ -0,0 +1,20 @@
+let quotes = [
+    'work is the curse of the drinking class',
+    'the only thing to fear is fear itself',
+    'fool me once shame on me, but teach a man to fool me and...'
+];
+
+let index = 0;
+
+// return on quote
+// not the same every time
+function getQuote() {
+    let quoteToReturn = quotes[index];
+    index++;
+    if (index >= quotes.length) {
+        index = 0;
+    }
+    return quoteToReturn;
+}
+
+module.exports = getQuote;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,19 @@
 // bring express package in from node_modules
 const express = require( 'express' );
+//bring in quotes module
+const getQuote = require( './modules/quotes.module.js' );
 
 // create an instance of express 
 const app = express( );
 // set port
 const PORT = 5000;
 
-let quotes = [
-    'work is the curse of the drinking class',
-    'the only thing to fear is fear itself',
-    'fool me once shame on me, but teach a man to fool me and...'
-    ];
-
 // setup basic route
 app.get( '/quote', function( req, res ){
     res.send( getQuote( ) )
 })
 
-let index = 0;
-// return on quote
-// not the same every time
-function getQuote( ){
-    let quoteToReturn = quotes[ index ];
-    index++;
-    if( index >= quotes.length ){
-        index = 0;
-    }
-    return quoteToReturn;
-}
-
-
 // start up server on port
 app.listen( PORT, function( ){
     console.log( `server is listening on PORT ${PORT}` );
-    
 })
